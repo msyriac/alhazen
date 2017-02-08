5554f4b0d1a4ed3a6571eb99ae53da21e01ec455
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-02-08 11:08:59 -0500

    arbitrary kappa lensing

--- tests/testLens.py
+++ tests/testLens.py
@@ -10,20 +10,18 @@ from orphics.tools.io import Plotter,dictFromSection,listFromConfig
 from szlib.szcounts import ClusterCosmology
 print "Done importing modules..."
 
+# === COSMOLOGY ===
 cosmologyName = 'LACosmology' # from ini file
-
 iniFile = "../SZ_filter/input/params.ini"
 Config = SafeConfigParser()
 Config.optionxform=str
 Config.read(iniFile)
-
 lmax = 5000
-
 cosmoDict = dictFromSection(Config,cosmologyName)
 constDict = dictFromSection(Config,'constants')
 cc = ClusterCosmology(cosmoDict,constDict,lmax)
 
-
+# === NFW CLUSTER ===
 massOverh = 2.e14
 concentration = 3.2
 zL = 0.7
@@ -32,49 +30,40 @@ overdensity = 180.
 critical = False
 atClusterZ = False
 
-
+# === TEMPLATE MAP ===
 px = 0.5
 arc = 300
 hwidth = arc/2.
-
 deg = utils.degree
 arcmin =  utils.arcmin
 shape, wcs = enmap.geometry(pos=[[-hwidth*arcmin,-hwidth*arcmin],[hwidth*arcmin,hwidth*arcmin]], res=px*arcmin, proj="car")
 thetaMap = enmap.posmap(shape, wcs)
 thetaMap = np.sum(thetaMap**2,0)**0.5
 
-# pl = Plotter()
-# pl.plot2d(thetaMap)
-# pl.done("output/theta.png")
 
-
-
+# === KAPPA MAP ===
 kappaMap,r500 = NFWkappa(cc,massOverh,concentration,zL,thetaMap*180.*60./np.pi,sourceZ,overdensity=overdensity,critical=critical,atClusterZ=atClusterZ)
-
 print "kappaint ", kappaMap[thetaMap*60.*180./np.pi<10.].mean()
 
 
+# === DEFLECTION MAP ===
 a = alphaMaker(thetaMap)
 alpha = a.kappaToAlpha(kappaMap)
-
-
 alphamod = 180.*60.*np.sum(alpha**2,0)**0.5/np.pi
 print "alphaint ", alphamod[thetaMap*60.*180./np.pi<10.].mean()
-
 pos = kappaMap.posmap() + alpha
 pix = kappaMap.sky2pix(pos, safe=False)
-        
+
+# === CMB POWER SPECTRUM ===      
 ps = powspec.read_spectrum("data/cl_lensinput.dat")
 
+
+# === RUN N SIMS ===      
 N = 100
-
-
-
-
 for i in range(N):
     print i
     map = enmap.rand_map(shape, wcs, ps)
-    lensedMap = lensing.displace_map(map, pix,order=5) #,lens_map(map, alpha)
+    lensedMap = lensing.displace_map(map, pix,order=5)
     
     if i==0:
         pl = Plotter()
