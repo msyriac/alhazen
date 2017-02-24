ddf6377282daca101cd1ff8f33d25edf1129e0a9
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-02-24 10:08:52 -0500

    minor

---
+++ scripts/subAll.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+post=$(date +%s)
+
+nohup wq sub -r "mode:bycore;N:${1};hostfile: auto;job_name: Nls;priority:med" -c "source ~/.bash_profile ; source ~/.bashrc  ; cd ~/repos/alhazen ; mpirun -hostfile %hostfile% python tests/testRecon.py $1" > output_$post.log &
+
