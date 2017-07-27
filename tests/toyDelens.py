454853a283c804195c8aad400f6ab1e7d411db9c
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-07-27 18:54:17 -0400

    minor

--- tests/toyDelens.py
+++ tests/toyDelens.py
@@ -33,7 +33,7 @@ lxmap_sim,lymap_sim,modlmap_sim,angmap_sim,lx_sim,ly_sim = fmaps.get_ft_attribut
 
 
 
-massOverh = 6.e14
+massOverh = 2.e14
 zL = 0.7
 overdensity = 500.
 critical = True
@@ -60,54 +60,76 @@ np.random.seed(200)
 # Unlensed
 unlensed = enmap.rand_map(shape_sim,wcs_sim,ps)
 
-
-pl = io.Plotter(scaleY='log')
-
-
-for k,lens_order in enumerate(range(5,0,-1)):
-
-    alpha = (lens_order-2+1.+1)/(5-2+1.+1)
-    print lens_order, alpha
-
-    # Lens with kappa1
-    lensed = lensing.lens_map_flat_pix(unlensed, alpha_pix,order=lens_order)
-
-    # Delens with kappa1
-    simple_delensed = lensing.lens_map_flat_pix(lensed.copy(), -alpha_pix,order=lens_order)
-
-    grad_phi = enmap.grad(phi)
-
-    lens_residual = lensed - unlensed
-    #io.quickPlot2d(lens_residual,out_dir+"lensres.png")
-    simple_residual = simple_delensed - unlensed
-    #io.quickPlot2d(simple_residual,out_dir+"simpleres.png")
+test = False
+if test:
+    pl = io.Plotter(scaleY='log')
 
 
-    iters = []
-    for nstep in range(1,8):
-        iter_delensed = lensing.delens_map(lensed.copy(), grad_phi, nstep=nstep, order=lens_order, mode="spline", border="cyclic")
-        # Check residual
+    for k,lens_order in enumerate(range(5,0,-1)):
 
-        iter_residual = iter_delensed - unlensed
-        iters.append(iter_residual)
+        alpha = (lens_order-2+1.+1)/(5-2+1.+1)
+        print lens_order, alpha
 
+        # Lens with kappa1
+        lensed = lensing.lens_map_flat_pix(unlensed, alpha_pix,order=lens_order)
 
-        #io.quickPlot2d(iter_residual,out_dir+"iterres_"+str(nstep).zfill(2)+".png")
+        # Delens with kappa1
+        simple_delensed = lensing.lens_map_flat_pix(lensed.copy(), -alpha_pix,order=lens_order)
 
+        grad_phi = enmap.grad(phi)
 
-    bin_edges = np.arange(0.,20.,0.5)
-    binner = stats.bin2D(modr_sim,bin_edges)
-
-    cents, prof = binner.bin(lens_residual)
-    pl.add(cents,np.abs(prof),ls="--",label=("no delensing" if k==0 else None),alpha=alpha,color="C0")
-    cents, prof = binner.bin(simple_residual)
-    pl.add(cents,np.abs(prof),ls="--",label=("anti-lensing"if k==0 else None),alpha=alpha,color="C1")
+        lens_residual = lensed - unlensed
+        #io.quickPlot2d(lens_residual,out_dir+"lensres.png")
+        simple_residual = simple_delensed - unlensed
+        #io.quickPlot2d(simple_residual,out_dir+"simpleres.png")
 
 
-    for i,iteration in enumerate(iters):
-        cents, prof = binner.bin(iteration)
-        pl.add(cents,np.abs(prof),label=("iterative nstep="+str(1+i) if k==0 else None),alpha=alpha,color="C"+str(i+2))
+        iters = []
+        for nstep in range(1,8):
+            iter_delensed = lensing.delens_map(lensed.copy(), grad_phi, nstep=nstep, order=lens_order, mode="spline", border="cyclic")
+            # Check residual
 
-        
-pl.legendOn(labsize=10)
-pl.done(out_dir+"iter_profs.png")
+            iter_residual = iter_delensed - unlensed
+            iters.append(iter_residual)
+
+
+            #io.quickPlot2d(iter_residual,out_dir+"iterres_"+str(nstep).zfill(2)+".png")
+
+
+        bin_edges = np.arange(0.,20.,0.5)
+        binner = stats.bin2D(modr_sim,bin_edges)
+
+        cents, prof = binner.bin(lens_residual)
+        pl.add(cents,np.abs(prof),ls="--",label=("no delensing" if k==0 else None),alpha=alpha,color="C0")
+        cents, prof = binner.bin(simple_residual)
+        pl.add(cents,np.abs(prof),ls="--",label=("anti-lensing"if k==0 else None),alpha=alpha,color="C1")
+
+
+        for i,iteration in enumerate(iters):
+            cents, prof = binner.bin(iteration)
+            pl.add(cents,np.abs(prof),label=("iterative nstep="+str(1+i) if k==0 else None),alpha=alpha,color="C"+str(i+2))
+
+
+    pl.legendOn(labsize=10)
+    pl.done(out_dir+"iter_profs.png")
+
+
+lens_order = 5
+nstep = 9
+
+iters = 2
+
+unlensed = unlensed * TCMB
+lensed = lensing.lens_map_flat_pix(unlensed, alpha_pix,order=lens_order)
+
+iter_delensed = lensed
+phi, fphi = lt.kappa_to_phi(kappa_map,modlmap_sim,return_fphi=True)
+grad_phi = enmap.grad(phi)/(iters)
+for i in range(1,iters+1):
+    print i
+    iter_delensed = lensing.delens_map(iter_delensed, grad_phi, nstep=nstep, order=lens_order, mode="spline", border="cyclic")
+    
+iter_residual = iter_delensed - unlensed
+lens_residual = lensed - unlensed
+io.quickPlot2d(lens_residual,out_dir+"lensres.png")
+io.quickPlot2d(iter_residual,out_dir+"iterres.png")
