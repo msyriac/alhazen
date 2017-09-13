377137a1838ce5f26e43c861ef791047109b221d
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-09-13 19:02:10 -0400

    maxlike

---
+++ alhazen/maxlike.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def lnlike(cov,covinv,stamp):
+
+    
+    Npix = stamp.size
+    assert covinv.size==Npix**2
+
+    # print "Calculating norm of lnlike..."
+    #lognorm = -0.5*(Npix*np.log(2.*np.pi)+np.trace(np.log(cov)))
+    lognorm = np.trace(np.log(cov))
+    print lognorm
+    
+    vec = stamp.reshape((Npix,1))
+
+    #quad = -np.dot(np.dot(vec.T,covinv),vec)
+    quad = np.dot(np.dot(vec.T,covinv),vec)
+    print quad
+
+    sys.exit()
+
+    ans = lognorm+quad
+    assert ans.size==1
+    return ans[0,0]
