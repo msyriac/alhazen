54a0e98920b6d3949fe789727b2d4e53f2baa681
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-10-28 03:57:24 -0400

    rotated clkk good to 1 percent

--- tests/test_rotate_power.py
+++ tests/test_rotate_power.py
@@ -1,71 +1,107 @@
+from orphics.tools.stats import bin2D
 from enlib import enmap, bench
 import orphics.tools.cmb as cmb
 import orphics.tools.io as io
 import numpy as np
 import sys
 import orphics.analysis.flatMaps as fmaps
+from orphics.analysis.pipeline import mpi_distribute, MPIStats
+from mpi4py import MPI
+
+Nsims = 1024
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+numcores = comm.Get_size()    
+Ntot = Nsims
+num_each,each_tasks = mpi_distribute(Ntot,numcores)
+mpibox = MPIStats(comm,num_each,tag_start=333)
+if rank==0: print "At most ", max(num_each) , " tasks..."
+my_tasks = each_tasks[rank]
+
+
 
 theory_file_root = "data/Aug6_highAcc_CDM"
 theory = cmb.loadTheorySpectraFromCAMB(theory_file_root,unlensedEqualsLensed=False,useTotal=False,TCMB = 2.7255e6,lpad=9000,get_dimensionless=False)
 
-wdeg = 45.
+wdeg = 40.
 hdeg = 15.
 yoffset = 60.
-pix = 4.0
-lmax = 2500
+pix = 1.0
+lmax = 5000
 shape,wcs = enmap.rect_geometry(width_arcmin=wdeg*60.,px_res_arcmin=pix,height_arcmin=hdeg*60.,yoffset_degree=yoffset)
 
 
 ells = np.arange(0,7000,1)
-ps = theory.lCl('TT',ells).reshape((1,1,7000))
+#pfunc = lambda x: theory.lCl('TT',x)
+#pfunc = lambda x: x*0.+1.
+pfunc = lambda x: theory.gCl('kk',x)
+ps = pfunc(ells).reshape((1,1,7000))
 
-with bench.show("Map generation"):
-    mg = enmap.MapGen(shape,wcs,ps)
-    taper,w2 = fmaps.get_taper(shape,taper_percent = 12.0,pad_percent = 3.0,weight=None)
-    map_south = mg.get_map()*taper
+mg = enmap.MapGen(shape,wcs,ps)
+taper,w2 = fmaps.get_taper(shape,taper_percent = 12.0,pad_percent = 3.0,weight=None)
+
+pxover = 1.0
+if rank==0:
+    with bench.show("Rot init..."):
+        r = fmaps.MapRotatorEquator(shape,wcs,wdeg,hdeg,width_multiplier=0.6,height_multiplier=1.2,downsample=True,verbose=True,pix_target_override_arcmin=pxover)
+else:
+    r = fmaps.MapRotatorEquator(shape,wcs,wdeg,hdeg,width_multiplier=0.6,height_multiplier=1.2,downsample=True,pix_target_override_arcmin=pxover)
     
-with bench.show("Rotation init"):
-    r = fmaps.MapRotatorEquator(map_south.shape,map_south.wcs,wdeg,hdeg,verbose=True,width_multiplier=0.6,height_multiplier=1.2,downsample=False)
-with bench.show("Rotation"):
+for k,index in enumerate(my_tasks):
+    map_south = mg.get_map(seed=index)*taper
+
+    
     rotmap = r.rotate(map_south)
-    del map_south
-    rottap = r.rotate(taper)
-    del taper
+
+    if k==0:
+        rottap = r.rotate(taper)
+        tmg = enmap.MapGen(r.shape_final,r.wcs_final,ps)
+        w2 = np.mean(rottap**2.)
+        fc = enmap.FourierCalc(r.shape_final,r.wcs_final)
+        modlmap = enmap.modlmap(r.shape_final,r.wcs_final)
+        bin_edges = np.arange(100,lmax,40)
+        binner = bin2D(modlmap,bin_edges)
+        
+    map_test = tmg.get_map(seed=index+int(1e6))*rottap
 
 
-with bench.show("Map generation"):
-    tmg = enmap.MapGen(r.shape_final,r.wcs_final,ps)
-    map_test = tmg.get_map()*rottap
-
-prefix = "Oct28_"
-# io.highResPlot2d(map_south,prefix+"smap.png")
-# io.highResPlot2d(rotmap,prefix+"rotmap.png")
-# io.highResPlot2d(rottap,prefix+"taper.png")
-# io.highResPlot2d(map_test,prefix+"testmap.png")
+    if rank==0 and k==0:
+        prefix = io.dout_dir+"Oct28_"
+        # io.highResPlot2d(map_south,prefix+"smap.png")
+        del map_south
+        # io.highResPlot2d(rotmap,prefix+"rotmap.png")
+        # io.highResPlot2d(rottap,prefix+"taper.png")
+        # io.highResPlot2d(map_test,prefix+"testmap.png")
 
 
-w2 = np.mean(rottap**2.)
 
-from orphics.tools.stats import bin2D
-fc = enmap.FourierCalc(r.shape_final,r.wcs_final)
-modlmap = enmap.modlmap(r.shape_final,r.wcs_final)
-p2d_rot,_,_ = fc.power2d(rotmap)/w2
-p2d,_,_ = fc.power2d(map_test)/w2
-p2d_theory = theory.lCl('TT',modlmap)
-
-bin_edges = np.arange(100,lmax,40)
-binner = bin2D(modlmap,bin_edges)
-
-cents,crot = binner.bin(p2d_rot)
-cents,ctest = binner.bin(p2d)
-cents,ctheory = binner.bin(p2d_theory)
+    p2d_rot,_,_ = fc.power2d(rotmap)/w2
+    p2d,_,_ = fc.power2d(map_test)/w2
 
 
-pl = io.Plotter()
-pl.add(cents,(crot-ctest)/ctest,label="rot vs test")
-pl.add(cents,(crot-ctheory)/ctheory,label="rot vs theory")
-pl.add(cents,(ctest-ctheory)/ctheory,label="test vs theory")
-pl.hline()
-pl.legendOn()
-pl.done(prefix+"cldiff.png")
+    cents,crot = binner.bin(p2d_rot)
+    cents,ctest = binner.bin(p2d)
+
+    mpibox.add_to_stack("crot",crot)
+    mpibox.add_to_stack("ctest",ctest)
+
+    if rank==0: print "Done with ", k+1," / ",len(my_tasks)
+
+mpibox.get_stacks()
+
+if rank==0:
+    p2d_theory = pfunc(modlmap)
+    cents,ctheory = binner.bin(p2d_theory)
+
+    crot = mpibox.stacks["crot"]
+    ctest = mpibox.stacks["ctest"]
+
+    pl = io.Plotter()
+    pl.add(cents,(crot-ctest)/ctest,label="rot vs test")
+    pl.add(cents,(crot-ctheory)/ctheory,label="rot vs theory")
+    pl.add(cents,(ctest-ctheory)/ctheory,label="test vs theory")
+    pl.hline()
+    pl.legendOn()
+    pl.done(prefix+"cldiff.png")
 
