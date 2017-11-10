0fd19482d045e0cb8f08ab9b4aef36f8a88dd961
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-11-10 12:38:25 -0800

    mpi

--- tests/test_rotate_power.py
+++ tests/test_rotate_power.py
@@ -7,6 +7,7 @@ import sys
 import orphics.analysis.flatMaps as fmaps
 from orphics.tools.mpi import MPI,mpi_distribute, MPIStats
 import healpy as hp
+from enlib import enmap
 
 Nsims = 16
 
@@ -32,7 +33,7 @@ ps = pfunc(ells).reshape((1,1,7000))
 wdeg = 40.
 hdeg = 15.
 yoffset = 60.
-pix = 1.0
+pix = 0.5
 lmax = 7000
 
 fshape, fwcs = enmap.fullsky_geometry(res=pix*np.pi/180./60., proj="car")
@@ -41,14 +42,16 @@ shape,wcs = enmap.rect_geometry(width_arcmin=wdeg*60.,px_res_arcmin=pix,height_a
 
 
 
-#mg = enmap.MapGen(shape,wcs,ps)
-taper,sw2 = fmaps.get_taper(shape,taper_percent = 18.0,pad_percent = 4.0,weight=None)
+with bench.show("taper"):
+    #mg = enmap.MapGen(shape,wcs,ps)
+    taper,sw2 = fmaps.get_taper(shape,taper_percent = 18.0,pad_percent = 4.0,weight=None)
 
 pxover = 1.0 #0.5
     
 for k,index in enumerate(my_tasks):
 
-    fullsky = curvedsky.rand_map(fshape, fwcs, ps, lmax=lmax,dtype=np.float32)
+    with bench.show("full_sky"):
+        fullsky = curvedsky.rand_map(fshape, fwcs, ps, lmax=lmax,dtype=np.float32)
 
 
     map_south = fullsky.submap(enmap.box(shape,wcs))*taper
